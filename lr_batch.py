a0712a26a12d79b505ff388d26245c97994b005e
刘钰婷 <liuyuting@liuyutiacstudio.lan>
2023-10-11 11:25:25 +0900

    add num_label check point

--- lr_batch.py
+++ lr_batch.py
@@ -11,40 +11,28 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report, log_loss, confusion_matrix
 from sklearn.feature_selection import SelectFromModel
+from collections import Counter
 
-# 设置文件夹路径
-# FOLDER_PATH = '/Users/liuyuting/WorkSpace/ScRNA_project/data_source/human_TabulaSapiens/exp/'
-# SAVE_PATH = '/Users/liuyuting/WorkSpace/ScRNA_project/data_source/human_TabulaSapiens/output/'
 
-"""这里的优化思路是使用temp_df读取完所有基因matrix并放入list中，在一起合并并删除重复cell_id，来取代迭代row的耗时做法"""
+def read_merge_file(filepath):
+    merged_df = pd.read_csv(filepath, header=0).fillna(0)
+    labels = merged_df.loc[:, 'label']
+    label_class = set(labels)
 
-# 合并文件夹内的数据
-def merge_data_in_folder(filepath):
-    # 读取文件夹内所有txt文件
-    filenames = [f for f in os.listdir(filepath) if f.endswith(".txt")]
+    # checkpoint1
+    if len(label_class) <= 1:
+        return None, None
+    # checkpoint2
+    for element, count in Counter(labels).items():
+        if count <= 10:
+            return None, None
 
-    dfs = []
-    label_dict = {}
-
-    for filename in filenames:
-        file_path = os.path.join(filepath, filename)
-        gene_name = filename.replace(".txt", "")
-
-        temp_df = pd.read_csv(file_path, sep="\t", header=None, names=['cell_id', gene_name, 'label'])  # names自定义列
-        label_dict.update(temp_df.set_index('cell_id')['label'].to_dict())  # {cell_id:label}
-
-        dfs.append(temp_df.set_index('cell_id').drop(columns='label'))
-
-    merged_df = pd.concat(dfs, axis=1)  # 横向拼接前set_index，相同cell_id自动合并
-
-    label_df = pd.DataFrame(list(label_dict.items()), columns=['cell_id', 'label']).set_index('cell_id')
-    merged_df = pd.concat([merged_df, label_df], axis=1).reset_index().rename(columns={'index': 'cell_id'})
     features = list(merged_df.columns)[1:-1]
-    print('merge success')
     return merged_df, features
+
 
 
 # 对数据进行标记
@@ -53,7 +41,7 @@ def label_data(merged_df):
     y = merged_df.iloc[:, -1].values
     le = preprocessing.LabelEncoder()
     y = le.fit_transform(y)
-    print('label success')
+
     return x, y
 
 
@@ -61,8 +49,7 @@ def label_data(merged_df):
 def train_and_evaluate(x, y, features):
     # split dataset
     x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, random_state=22, test_size=0.3)
-    print('split sucess')
-    
+
     # Scaling
     scaler = MinMaxScaler()
     x_train_scaled = scaler.fit_transform(x_train)
@@ -74,7 +61,7 @@ def train_and_evaluate(x, y, features):
     grid_search = GridSearchCV(LogisticRegression(), param_grid, cv=5)
     grid_search.fit(x_train_scaled, y_train)
     print('grid search sucess')
-    
+
     # train with searched parameter
     best_estimator = grid_search.best_estimator_
     print(best_estimator)
@@ -86,7 +73,7 @@ def train_and_evaluate(x, y, features):
           'After feature selection shape:', x_train_sfm.shape)
     lr = LogisticRegression(max_iter=1000)
     lr.fit(x_train_sfm, y_train)
-    
+
     # evaluation on validation set
     y_pred = lr.predict(x_test_sfm)
     y_pred_prob = lr.predict_proba(x_test_sfm)[:, 1]
@@ -98,7 +85,6 @@ def train_and_evaluate(x, y, features):
 
 
 def report_coef(model, features, feature_mask):
-
     coef = model.coef_
 
     # 对数组进行排序
@@ -116,24 +102,32 @@ def report_coef(model, features, feature_mask):
 
 # 主逻辑
 def main(argv):
+
     FOLDER_PATH = argv[0]
     SAVE_PATH = argv[1]
-    cell_types = [foldername for foldername in os.listdir(FOLDER_PATH) if not foldername.startswith('.')]
+    FILE_END = argv[2]
+
+    cell_types = [foldername for foldername in os.listdir(FOLDER_PATH) if not foldername.startswith('.') and foldername.endswith(FILE_END)]
     reports = []
     coef_reports = {}
+    missing_class_file = []
     for cell_type in cell_types:
         print(f"{cell_type} start process")
         filepath = os.path.join(FOLDER_PATH, cell_type)
-        merged_df, features = merge_data_in_folder(filepath)
+        merged_df, features = read_merge_file(filepath)
+        # check point 1
+        if merged_df is None and features is None:
+            missing_class_file.append(cell_type)
+            print(f"Skipped processing for missing label class: {cell_type}")
+            continue  # Continue with the next iteration of the loop
 
         x, y = label_data(merged_df)
         test_loss, conf_mat, report, coef_report, estimator = train_and_evaluate(x, y, features)
 
-        # 将结果存入报告列表
         coef_reports.update({cell_type: coef_report})
 
         reports.append({
-            'cell_type': cell_type,
+            'cell_type': cell_type.rstrip('_merge.csv'),
             'num_samples': len(x),
             'num_genes': x.shape[1],
             'select_genes': len(coef_report['gene']),
@@ -145,16 +139,20 @@ def main(argv):
         })
         print(f"{cell_type} complete, accuracy={report['accuracy']}")
 
+    # save info
     df = pd.DataFrame(reports)
     df.to_csv(f'{SAVE_PATH}/sum_report.csv', index=False)
     with open(f'{SAVE_PATH}/coef_reports.json', 'w') as j:
         json.dump(coef_reports, j)
+    if len(missing_class_file) > 0:
+        with open(f'{SAVE_PATH}/missing_class_file.json', 'w') as j:
+            json.dump(missing_class_file, j)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python ./main.py {DATA_FOLDER_PATH} {SAVE_PATH}')
-        print('Example: DATA_FOLDER_PATH = /Users/human_TabulaSapiens/exp/ SAVE_PATH = /Users/human_TabulaSapiens/output/')
+    if len(sys.argv) != 4:
+        print('Usage: python ./main.py {DATA_FOLDER_PATH} {SAVE_PATH} {FILE_END}')
+        print(
+            'Example: DATA_FOLDER_PATH = /Users/human_TabulaSapiens/exp/ SAVE_PATH = /Users/human_TabulaSapiens/output/ FILE_END=_merge.csv' )
         sys.exit(1)
     main(sys.argv[1:])
-
