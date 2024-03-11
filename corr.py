f53dbdc957081569043d4a3ab3364844fb5d0992
LiuLime <liuyuting0219@gmail.com>
2024-03-11 18:26:28 +0900

    update corr.py

--- corr.py
+++ corr.py
@@ -50,31 +50,11 @@ def save_graph_object(G, name):
     nx.write_graphml(G, f"{name}.graphml")
 
 
-# def graph_laplacian_matrix(G):
-#     """calculate graph feature by laplacian matrix"""
-#     # 计算拉普拉斯矩阵
-#     L = nx.laplacian_matrix(G).toarray()
-#     # 计算特征值
-#     eigvals = np.linalg.eigvals(L)
-#     # 排序特征值
-#     eigvals_sorted = np.sort(eigvals)
-#     return eigvals_sorted
-#
-#
-# def graph_distance(G1, G2):
-#     """计算两个图的距离 graph similarity（distance）"""
-#     eigvals1_sorted = graph_laplacian_matrix(G1)
-#     eigvals2_sorted = graph_laplacian_matrix(G2)
-#     # 比较特征值（这里使用欧式距离计算谱距离）
-#     distance = np.linalg.norm(eigvals1_sorted - eigvals2_sorted)
-#     return distance
-
-
-def create_edge_tuple(x):
+def generate_edge_attributes(x):
     return tuple([x["gene1"], x["gene2"], {"weight": x["cor_pearson.binMean"], "cell_n": x["cell_n"]}])
 
 
-def create_df_network(df, group_level, fig_save_path, markers):
+def generate_network_from_dataframe(df, group_level, fig_save_path, markers):
     """
     Create network from dataframe
     param:
@@ -92,7 +72,7 @@ def create_df_network(df, group_level, fig_save_path, markers):
         case _:
             raise TypeError("invalid group_level argument, use 'tissue' 'cell'")
 
-    df.loc[:, "edge_attr"] = df.apply(lambda x: create_edge_tuple(x), axis=1)
+    df.loc[:, "edge_attr"] = df.apply(lambda x: generate_edge_attributes(x), axis=1)
     result = df.groupby(by=groupby)["edge_attr"].apply(lambda x: x.tolist()).reset_index()
     result.to_csv(f"{fig_save_path}/temp_result.csv", index=False)
     graph_list = []
@@ -106,6 +86,52 @@ def create_df_network(df, group_level, fig_save_path, markers):
     return graph_list
 
 
+@staticmethod
+def is_marker(x, markers):
+    if x in markers:
+        return True
+    else:
+        return False
+
+
+def generate_count_from_dataframe(df, group_level, fig_save_path, markers):
+    match group_level:
+        case "tissue":
+            groupby = ["study", "group", "tissue", "gene1"]
+        case "cell":
+            groupby = ["study", "group", "tissue", "cellType", "gene1"]
+        case _:
+            raise TypeError("invalid group_level argument, use 'tissue' 'cell'")
+
+    result = df.groupby(by=groupby)["gene2"].count().reset_index()
+    result["is_marker"] = result["gene1"].apply(is_marker, markers=markers)
+    result.to_csv(f"{fig_save_path}/marker_degree.csv", index=False)
+    return result
+
+
+def create_graphs_from_csv_data(folder_path, file, group_level, fig_save_path, **kwargs):
+    """
+    param:
+        * kwargs: "corr_shrefold", "p_shrefold"
+    """
+    marker_tissue_corr_data_path = os.path.join(folder_path, file)
+    with open(marker_tissue_corr_data_path, "r") as p:
+        marker_tissue_corr_data = pd.read_csv(p, sep="\t", header=0)
+
+    # correlation shrefold & p-value shrefold
+    marker_tissue_corr_slice = marker_tissue_corr_data[
+        marker_tissue_corr_data["cor_pearson.binMean"] > kwargs["corr_shrefold"]]
+    marker_tissue_corr_slice = marker_tissue_corr_slice[marker_tissue_corr_slice["p1"] < kwargs["p_shrefold"]]
+    marker_tissue_corr_slice = marker_tissue_corr_slice[marker_tissue_corr_slice["p2"] < kwargs["p_shrefold"]]
+    graph_list = generate_network_from_dataframe(marker_tissue_corr_slice, group_level=group_level,
+                                                 fig_save_path=fig_save_path,
+                                                 markers=markers)
+    marker_degree = generate_count_from_dataframe(marker_tissue_corr_slice, group_level=group_level,
+                                                  fig_save_path=fig_save_path,
+                                                  markers=markers)
+    return graph_list, marker_degree
+
+
 # %%
 with open("./config.json") as j:
     config = json.load(j)
@@ -114,86 +140,79 @@ fig_save_path = config["fig_save_path"]
 corrDatafilebymedian = config["corrDataFolderBymedian"]
 corrDatafileby4060 = config["corrDataFolderBy40-60"]
 corr_shrefold = config["corr_shrefold"]
+p_shrefold = config["p_shrefold"]
 markers = config["markers"]
 TissueMarkerCorrFile = "tissue_level/agingMarkers-related_genePairCor_pLT0.2_1ageGrpGE50genes.txt"
 CellMarkerCorrFile = "tissue-cellType_level/agingMarkers-related_genePairCor_pLT0.2_1ageGrpGE50genes.txt"
 
 """
-Aging markers-related correlation network <- median age
+Aging markers-related correlation&p-value network <- median age
+p-value使用的双边检的值
 """
 
 # Tissue level
+create_graphs_from_csv_data(corrDatafilebymedian,
+                            TissueMarkerCorrFile,
+                            group_level="tissue",
+                            fig_save_path=fig_save_path + "tissue_level_medianAge_corr0.7_p0.001/",
+                            corr_shrefold=corr_shrefold,
+                            p_shrefold=p_shrefold,
+                            )
 # marker_tissue_corr_data_path = os.path.join(corrDatafilebymedian, TissueMarkerCorrFile)
 # marker_tissue_corr_data = pd.read_csv(marker_tissue_corr_data_path, sep="\t", header=0)
-# # correlation shrefold
+# # correlation shrefold & p-value shrefold
 # marker_tissue_corr_slice = marker_tissue_corr_data[marker_tissue_corr_data["cor_pearson.binMean"] > corr_shrefold]
+# marker_tissue_corr_slice = marker_tissue_corr_slice[marker_tissue_corr_slice["p1"] < p_shrefold]
 # graph_list = create_df_network(marker_tissue_corr_slice, group_level="tissue", fig_save_path=fig_save_path, markers=markers)
 
 # Cell level
+create_graphs_from_csv_data(corrDatafilebymedian,
+                            CellMarkerCorrFile,
+                            group_level="cell",
+                            fig_save_path=fig_save_path + "cell_level_medianAge_corr0.7_p0.001/",
+                            corr_shrefold=corr_shrefold,
+                            p_shrefold=p_shrefold)
+
 # marker_cell_corr_data_path = os.path.join(corrDatafilebymedian, CellMarkerCorrFile)
 # marker_cell_corr_data = pd.read_csv(marker_cell_corr_data_path, sep="\t", header=0)
 #
 # marker_cell_corr_slice = marker_cell_corr_data[marker_cell_corr_data["cor_pearson.binMean"] > corr_shrefold]
+# marker_cell_corr_slice = marker_cell_corr_slice[marker_cell_corr_slice["p1"] < p_shrefold]
 # graph_list = create_df_network(marker_cell_corr_slice, group_level="cell", fig_save_path=fig_save_path, markers=markers)
 
 """
 Aging markers-related correlation network <- 40-60 age
 """
 # Tissue level
+create_graphs_from_csv_data(corrDatafileby4060,
+                            TissueMarkerCorrFile,
+                            group_level="tissue",
+                            fig_save_path=fig_save_path + "tissue_level_40-60Age_corr0.7_p0.001/",
+                            corr_shrefold=corr_shrefold,
+                            p_shrefold=p_shrefold,
+                            )
+
 # marker_tissue_corr_data_path = os.path.join(corrDatafileby4060, TissueMarkerCorrFile)
 # marker_tissue_corr_data = pd.read_csv(marker_tissue_corr_data_path, sep="\t", header=0)
 # # correlation shrefold
 # marker_tissue_corr_slice = marker_tissue_corr_data[marker_tissue_corr_data["cor_pearson.binMean"] > corr_shrefold]
+# marker_tissue_corr_slice = marker_tissue_corr_slice[marker_tissue_corr_slice["p1"] < p_shrefold]
 # graph_list = create_df_network(marker_tissue_corr_slice, group_level="tissue", fig_save_path=fig_save_path, markers=markers)
 
 # Cell level
+create_graphs_from_csv_data(corrDatafileby4060,
+                            CellMarkerCorrFile,
+                            group_level="cell",
+                            fig_save_path=fig_save_path + "cell_level_40-60Age_corr0.7_p0.001/",
+                            corr_shrefold=corr_shrefold,
+                            p_shrefold=p_shrefold)
+
 # marker_cell_corr_data_path = os.path.join(corrDatafileby4060, CellMarkerCorrFile)
 # marker_cell_corr_data = pd.read_csv(marker_cell_corr_data_path, sep="\t", header=0)
 #
 # marker_cell_corr_slice = marker_cell_corr_data[marker_cell_corr_data["cor_pearson.binMean"] > corr_shrefold]
-# graph_list = create_df_network(marker_cell_corr_slice, group_level="cell", fig_save_path=fig_save_path, markers=markers)
+# marker_cell_corr_slice = marker_cell_corr_slice[marker_cell_corr_slice["p1"] < p_shrefold]
+# graph_list = generate_network_from_dataframe(marker_cell_corr_slice, group_level="cell", fig_save_path=fig_save_path,
+#                                              markers=markers)
 
-# %%
-import ast
-import MySQL
-
-mysql = MySQL.sql()
-
-
-def query_mysql(query_tissue, marker, target):
-    with mysql as sql:
-        cell_id = sql.search(table=query_tissue, define_column="gene_name", query_column="cell_id", query=marker)[0]
-        marker_exp = \
-            sql.search(table=query_tissue, define_column="gene_name", query_column="gene_expression", query=marker)[0]
-
-        target_exp = sql.search(table=query_tissue, define_column=["cell_id", "gene_name"],
-                                query_column="gene_expression",
-                                query=[cell_id, target])
-        if target_exp:
-            target_exp = target_exp[0]
-    return cell_id, target_exp, marker_exp
-
-
-test_file = "/Users/liuyuting/WorkSpace/ScRNA_project/03figure/cell_level_medianAge_corr0.7/temp_result.csv"
-# test 17.human_TabulaSapiens normal Bladder Fibroblast
-with open(test_file, "r") as t:
-    data = pd.read_csv(t, header=0)
-idx = 11
-edges = data.loc[idx, "edge_attr"]
-study=data.loc[idx, 'study']
-tissue=data.loc[idx, 'tissue']
-cellType=data.loc[idx, 'cellType']
-print(f"testing {study,tissue , cellType}")
-edges = ast.literal_eval(edges)
-project_list = []
-for tuples in edges:
-    target = tuples[0]
-    marker = tuples[1]
-    cell_id, gene_exp, marker_exp = query_mysql(tissue+"_cell", marker, target)
-    projection = {"marker": marker, "target": target, "cell_id": cell_id, "marker_exp": marker_exp,
-                  "target_exp": gene_exp}
-    project_list.append(projection)
-    # print(project_list)
-project_df = pd.DataFrame(project_list)
-project_df.to_csv(f"{fig_save_path}/exp_result_bladder_fibroblast.csv", index=False)
 # %%
