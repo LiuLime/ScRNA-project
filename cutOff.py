e2927132ebff2235a70fe983dd8bf4b5406aea14
LiuLime <liuyuting0219@gmail.com>
2024-04-08 10:43:37 +0900

    update database fetch script

--- cutOff.py
+++ cutOff.py
@@ -15,29 +15,37 @@ def save_json(data, path):
 
 
 class QueryDB:
-    def __init__(self, database, p_threshold, corr_threshold):
+    def __init__(self, database):
         self.db = db.databaseHelper(database)
         self.db.init()
         self.log = logger()
-        self.log10p_dict = {}
+
+
+    def set_filter(self, p_threshold, corr_threshold):
+        """init p threshold, correlation threshold and log10p records above p threshold in the database"""
         self.p_threshold = p_threshold
         self.corr_threshold = corr_threshold
+        self.log10p_dict_list = self.get_log10p_dict_in_all_studies()
 
     def get_avaliable_studies(self) -> list:
+        """extract all abbr_id tables in the database"""
         _, tasks = self.db.get_available_tasks()
         tasks = [i["Table"] for i in tasks]
         self.log.debug(f"available_studies--->{len(tasks)}:{tasks}")
         return tasks
 
     def get_log10p_dict_in_all_studies(self) -> list[dict]:
-        flag, self.log10p_dict = self.db.get_log10p_records_above_threshold(self.p_threshold)
-        self.log.debug(f"get_log10p_dict_in_all_studies---> flag {flag}")
-        return self.log10p_dict
+        """extract list of log10p records above p_shreshold in the database"""
+
+        flag, self.log10p_dict_list = self.db.get_log10p_records_above_threshold(self.p_threshold)
+        self.log.debug(f"log10p_dict init {flag}")
+        return self.log10p_dict_list
 
     def search_in_log10p_dict(self, abbr_id, gene) -> str | None:
         """check if pvalue of correlation gene pairs over p_threshold"""
-        log10p_dict_abbr = [record for record in self.log10p_dict if record["abbr_id"] == abbr_id]
-        gene_dict = {record["gene"]: record["log10p"] for record in log10p_dict_abbr}
+
+        gene_dict = {record["gene"]: record["log10p"] for record in self.log10p_dict_list if record["abbr_id"] == abbr_id}
+        # print("gene_dict keys", len(gene_dict.keys()))
         if gene in gene_dict.keys():
             return gene_dict[gene]
         else:
@@ -51,12 +59,14 @@ class QueryDB:
             self.log.debug(f"--- get corr_dict_in_one_study {abbr_id} ---> flag {flag}")
 
             for record in corr_dict_list:
+                # print("record waiting search", record)
                 gene1_log10p = self.search_in_log10p_dict(abbr_id, record["gene1"])
                 gene2_log10p = self.search_in_log10p_dict(abbr_id, record["gene2"])
                 if gene1_log10p and gene2_log10p:
                     record["log10p1"] = gene1_log10p
                     record["log10p2"] = gene2_log10p
                     record["abbr_id"] = abbr_id
+                    # print("record searched", record)
                     new_corr_dict_list.append(record)
                 else:
                     pass
@@ -91,12 +101,12 @@ save_title_df = "screen.csv"
 
 for database, save_path in zip(db_list, figSavePath.values()):
     logger().log.debug(f"USE {database}, save path--->{save_path}")
+    query = QueryDB(database)
     for p in log10p_abs_cutoffs:
         for c in corr_cutoffs:
             folder = f"log10p_{p}_corr_{c}/"
             common.create_folder(save_path + folder)
-
-            query = QueryDB(database, p, c)
+            query.set_filter(p, c)
             corr_dict = query.get_corr_dict_in_all_study(save_path + folder, save_json_file=True)
 
             # df = pd.DataFrame(corr_dict)
